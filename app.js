717d1cdc55302e332ea272b2ada68d532d0aa0cc
SantiagoFunes <santiagofunes.97@hotmail.com>
2021-11-20 15:47:18 -0300

    Pagina Inicio

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
