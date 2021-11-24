74dce5d40cdd29ffaf2088f9202c220f4af404a2
lucas yalet <lucasyalet94Qgmail.com>
2021-11-23 22:45:40 -0300

    agregando retro

---
+++ RETRO.md
@@ -0,0 +1,5 @@
+# Conclusiones del primer Sprint
+- ¿Que hicimos bien?
+- ¿Que hicimos mal?
+-¿Que deberiamos empezar a hacer?
+-¿Que deberiamos dejar de hacer?
--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const express = require("express")
+const app = express()
+const path = require("path");
+const exp = require("constants");
+app.use(express.urlencoded({extended: true}))
 
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+const publicpath = path.resolve(__dirname,'./public');
+app.use(express.static(publicpath))
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+app.get("/",(req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
 
-app.post('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+app.get("/register",(req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+app.get("/login",(req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log("Servidor funcionando");
+})
