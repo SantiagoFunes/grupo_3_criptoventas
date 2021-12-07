81f87083704f373ed945a226489b9a5b02f3957f
Chorcas94 <tellecheaestebanm@gmail.com>
2021-12-07 17:38:26 -0300

    links

--- src/app.js
+++ src/app.js
@@ -5,28 +5,28 @@ const exp = require("constants");
 app.use(express.urlencoded({extended: true}))
 
 
-const publicpath = path.resolve(__dirname,'./public');
+const publicpath = path.resolve(__dirname,'../public');
 app.use(express.static(publicpath))
 
 
 app.get("/",(req,res) => {
-    res.sendFile(path.resolve(__dirname, './products/home.html'))
+    res.sendFile(path.resolve(__dirname, './views/products/home.html'))
 })
 
 app.get("/register",(req,res) => {
-    res.sendFile(path.resolve(__dirname, './users/register.html'))
+    res.sendFile(path.resolve(__dirname, './views/users/register.html'))
 })
 
 app.get("/login",(req,res) => {
-    res.sendFile(path.resolve(__dirname, './users/login.html'))
+    res.sendFile(path.resolve(__dirname, './views/users/login.html'))
 })
 
 app.get("/carrito",(req,res) => {
-    res.sendFile(path.resolve(__dirname, './products/carrito.html'))
+    res.sendFile(path.resolve(__dirname, './views/products/carrito.html'))
 })
 
 app.get("/detalle:id",(req,res) => {
-    res.sendFile(path.resolve(__dirname, './products/detalle.html'))
+    res.sendFile(path.resolve(__dirname, './views/products/detalle.html'))
 })
 
 app.listen(process.env.PORT || 3000, ()=> {
