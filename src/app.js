cbddf5c5d2c7f556a179ff22f0b92d762e0f5eab
Chorcas94 <tellecheaestebanm@gmail.com>
2021-12-13 22:02:02 -0300

    app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const path = require("path");
 const exp = require("constants");
+const { json } = require("express");
 app.use(express.urlencoded({extended: true}))
 
 app.set('views', path.join(__dirname, '/views'));
@@ -29,7 +30,54 @@ app.get("/carrito",(req,res) => {
 app.get("/productos",(req,res) => {
     res.render('products/productos')
 })
+const productList=[
+    {
+        id:1,
+        name:"Aeorus",
+        price:27000,
+    },
+    {
+        id:2,
+        name:"Gskill",
+        price:37000,
+    },
+    {
+        id:3,
+        name:"Ram",
+        price:1000,
+    }
+]
+app.get("/productos/:id", (req,res)=>{
+    const paramId= parseInt(req.params.id, 10);
+    const product= productList.find(productElement => productElement.id === paramId)
+    if(product != null)
+    {
+        res.send(product);
+    }
+    else
+    {
+        res.status(404).json({msg: "No se encuentra el producto"})
+    }
+})
+app.delete("/productos/:id", (req,res)=>{
+    const paramId= parseInt(req.params.id, 10);
+    const productIndex= productList.findIndex(productElement => productElement.id === paramId)
 
+    productList.splice(productIndex,1)
+
+    res.json({msg:'Product deleted'});
+})
+app.post("/productos/:id", (req,res)=>{
+    const {name, price}=req.body;
+    const productId=productList[productList.length -1].id +1;
+    const product={
+        id:productId,
+        name,
+        price
+    }
+    productList.push(product);
+    res.status(201).json(product)
+})
 app.get("/detalle",(req,res) => {
     res.render('products/detalle')
 })
