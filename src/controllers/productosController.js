a5174dd6796f4822ede04aab1329e4469f2264fd
Chorcas94 <tellecheaestebanm@gmail.com>
2021-12-15 22:18:35 -0300

    pusheo rutas

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -4,9 +4,9 @@ module.exports=productosController;
 const path = require('path')
 
 const productsControllers = {
-    detail:(req, res) => {res.render(path.resolve("./views/products/detalle"))},
-    allProducts:(req, res) => {res.render(path.resolve('./views/products/productos'))},
-    editor:(req, res) => {res.render(path.resolve('./views/products/editor'))},
+    detail:(req, res) => {res.render("products/detalle")},
+    allProducts:(req, res) => {res.render('products/productos')},
+    editor:(req, res) => {res.render('products/editor')},
     detalleId:(req, res) => {const paramId= parseInt(req.params.id, 10);
         const product= productList.find(productElement => productElement.id === paramId)
         if(product != null)
