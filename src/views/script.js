fc685dda9a42bfa1419a587653d27f3736c35a1e
Fabricio <fabriciogodoy95hotmail.com>
2022-03-18 15:38:26 -0300

    validacion del lado del Front

--- src/views/script.js
+++ src/views/script.js
@@ -1,5 +1,3 @@
-
-
 // validacion por frontend
 
 const form = document.getElementById('formularioValidado');
@@ -57,19 +55,3 @@ function checkInputs() {
 		setSuccessFor(password2);
 	}
 }
-
-// function setErrorFor(input, message) {
-// 	const formControl = input.parentElement;
-// 	const small = formControl.querySelector('small');
-// 	formControl.className = 'form-control error';
-// 	small.innerText = message;
-// }
-
-// function setSuccessFor(input) {
-// 	const formControl = input.parentElement;
-// 	formControl.className = 'form-control success';
-// }
-	
-// function isEmail(email) {
-// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-// }
